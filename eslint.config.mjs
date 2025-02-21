def3b18c430ce69cf9ef81e26bcccbb7a730b47a
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-02-21 03:28:48 -0600

    add custom rule -no-unussed vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unsued-vars": "0",
+    },
+  },
 ];
